214b292a113d56522b4453e9c8dad25a0bce19d8
Dorstol <olehsofronov@gmail.com>
2023-12-12 14:32:16 +0200

    add Profile and form for edit

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ['user']
+    list_display = ['user', 'date_of_birth', 'photo']
