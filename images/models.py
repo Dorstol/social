d79bf7fb866c62f794bc4e5d2651ab4b5a571462
Dorstol <olehsofronov@gmail.com>
2023-12-18 17:49:14 +0200

    added image app

---
+++ images/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+
+
+class Image(models.Model):
+    user = models.ForeignKey(User, related_name="image_created",
+                             on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=128)
+    description = models.TextField(blank=True)
+    url = models.URLField(max_length=2000)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    created = models.DateField(auto_now_add=True)
+    users_like = models.ManyToManyField(User,
+                                        related_name="images_liked",
+                                        blank=True)
+
+    class Meta:
+        indexes = models.Index(fields=['-created']),
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
